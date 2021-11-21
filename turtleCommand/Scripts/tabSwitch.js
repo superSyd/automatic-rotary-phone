402bc1c7dd163d8c0116de226266a5d99bab8769
superSyd <37779021+superSyd@users.noreply.github.com>
2021-11-21 13:05:42 +1100

    Add files via upload

---
+++ turtleCommand/Scripts/tabSwitch.js
@@ -0,0 +1,16 @@
+﻿//Inspired from https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function loadForm(event, FormName) {
+    var i, tabcontent, taboptions;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    taboptions = document.getElementsByClassName("taboptions");
+    for (i = 0; i < taboptions.length; i++) {
+        taboptions[i].className = taboptions[i].className.replace(" active", "");
+    }
+    document.getElementById(FormName).style.display = "block";
+    event.currentTarget.className += " active";
+
+}
